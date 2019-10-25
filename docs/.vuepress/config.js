73d6facb77715c887f8637f3c01f192d03a87b84
KennyWho <princesswar721@gmail.com>
2019-10-25 20:01:33 +0800

    增加文章

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const blogs = path.resolve(".", "./docs/api/blogs.json");
 const daily = path.resolve(".", "./docs/api/daily.json");
+const notes = path.resolve(".", "./docs/api/notes.json");
 delete require.cache[blogs];
 delete require.cache[daily];
 function getResult(json, key) {
@@ -19,11 +20,13 @@ module.exports = {
     nav: [
       { text: "Blogs", link: "/blogs/" },
       { text: "Daily", link: "/daily/" },
+      { text: "Notes", link: "/notes/" },
       { text: "Github", link: "https://github.com/KennyWho" }
     ],
     sidebar: {
       "/blogs/": getResult(blogs, "blogs"),
-      "/daily/": getResult(daily, "daily")
+      "/daily/": getResult(daily, "daily"),
+      "/notes/": getResult(notes, "notes")
     }
   }
 };
