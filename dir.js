e269b94291f4ca89934ac6e1a50287ac1995be92
x-ray <princesswar721@gmail.com>
2022-12-09 19:22:22 +0800

    modify dir

--- dir.js
+++ dir.js
@@ -5,11 +5,11 @@ const readdir = util.promisify(fs.readdir)
 
 async function postApi(_path) {
   const posts = await readdir(path.resolve('./', `./docs/${_path}`))
-  const markdowns = posts.filter(fs => /\.md$/.test(fs))
+  const markdowns = posts.filter((fs) => /\.md$/.test(fs))
   const data = {
     list: markdowns,
   }
-  fs.writeFile(path.resolve('./', `./docs/api/${_path}.json`), JSON.stringify(data), err => {
+  fs.writeFile(path.resolve('./', `./docs/api/${_path}.json`), JSON.stringify(data), (err) => {
     if (err) {
       throw err
     }
@@ -19,4 +19,3 @@ postApi('blogs')
 postApi('daily')
 postApi('notes')
 postApi('daily-interview-question')
-postApi('tech')
